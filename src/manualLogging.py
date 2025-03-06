8ba51a21d0bd5dada8ca15b3fd601da4252c3637
aliyan16 <aliyanm12376@gmail.com>
2025-03-06 21:30:05 +0500

    added manual logging code and mlflow setup

---
+++ src/manualLogging.py
@@ -0,0 +1,30 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+
+wine=load_wine()
+x=wine.data
+y=wine.target
+
+
+xtrain,xtest,ytrain,ytest=train_test_split(x,y,test_size=0.3,random_state=42)
+max_depth=5
+n_estimators=10
+
+with mlflow.start_run():
+    rf=RandomForestClassifier(max_depth=max_depth,n_estimators=n_estimators,random_state=42)
+    rf.fit(xtrain,ytrain)
+    ypred=rf.predict(xtest)
+    accuracy=accuracy_score(ypred,ytest)
+    mlflow.log_metric('accuracy',accuracy)
+    mlflow.log_param('max depth',max_depth)
+    mlflow.log_param('n estimators',n_estimators)
+    
