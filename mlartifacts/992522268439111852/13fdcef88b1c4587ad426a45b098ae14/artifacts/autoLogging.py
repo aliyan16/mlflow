8f95f8f118155b2ac4c34797fa9ffd11e9880f8b
aliyan16 <aliyanm12376@gmail.com>
2025-03-07 06:29:00 +0500

    Remote setup and autoLogging code

---
+++ mlartifacts/992522268439111852/13fdcef88b1c4587ad426a45b098ae14/artifacts/autoLogging.py
@@ -0,0 +1,47 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+# import dagshub
+
+# dagshub.init(repo_owner='aliyan16', repo_name='mlflow', mlflow=True)
+
+# mlflow.set_tracking_uri('https://dagshub.com/aliyan16/mlflow.mlflow')
+mlflow.set_tracking_uri('http://127.0.0.1:5000')
+
+wine=load_wine()
+x=wine.data
+y=wine.target
+
+
+xtrain,xtest,ytrain,ytest=train_test_split(x,y,test_size=0.3,random_state=42)
+max_depth=7
+n_estimators=10
+
+rf=RandomForestClassifier(max_depth=max_depth,n_estimators=n_estimators,random_state=42)
+rf.fit(xtrain,ytrain)
+ypred=rf.predict(xtest)
+accuracy=accuracy_score(ypred,ytest)
+confusionMat=confusion_matrix(ypred,ytest)
+plt.figure(figsize=(6,6))
+sns.heatmap(confusionMat,annot=True,fmt='d',cmap='Blues',xticklabels=wine.target_names,yticklabels=wine.target_names)
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.savefig('ConfusionMat.png')
+
+# mlflow.set_experiment('Experiment1')
+mlflow.autolog()
+
+with mlflow.start_run(experiment_id=992522268439111852):
+    # mlflow.log_metric('accuracy',accuracy)
+    # mlflow.log_param('max depth',max_depth)
+    # mlflow.log_param('n estimators',n_estimators)
+    # mlflow.log_artifact('confusionMat.png')
+    mlflow.log_artifact(__file__)
+    mlflow.set_tags({'Dev':'Aliyan','Project':'Classification'})
+    # mlflow.sklearn.log_model(rf,'Random Forest')
+
